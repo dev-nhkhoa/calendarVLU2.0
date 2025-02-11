08ff103a1a725febf6faafa860d25de7eb46444b
dev-nhkhoa <work.nhkhoa@gmail.com>
2025-02-11 17:55:59 +0700

    Fix import Header & Footer for convert

--- src/app/convert/layout.tsx
+++ src/app/convert/layout.tsx
@@ -1,13 +1,13 @@
 import type React from 'react'
-import { Header } from '@/components/header'
-import Footer from '@/components/footer'
+import HeaderPage from '@/components/header'
+import FooterPage from '@/components/footer'
 
 export default function ConvertLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex flex-col min-h-screen min-w-screen justify-between">
-      <Header />
+      <HeaderPage />
       {children}
-      <Footer />
+      <FooterPage />
     </main>
   )
 }
--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,13 +1,13 @@
+import FooterPage from '@/components/footer'
+import HeaderPage from '@/components/header'
 import type React from 'react'
-import { Header } from '@/components/header'
-import Footer from '@/components/footer'
 
 export default function SettingsLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex flex-col min-h-screen min-w-screen justify-between">
-      <Header />
+      <HeaderPage />
       {children}
-      <Footer />
+      <FooterPage />
     </main>
   )
 }

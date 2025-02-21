439c980bdde1c6c3d389d343703f4300831820d3
TRUONG NGUYEN ANH KHOA <work.nhkhoa@gmail.com>
2025-02-21 15:50:14 +0700

    Add VLU login API endpoint and remove deprecated check-vlu-account route

---
+++ src/app/api/accounts/vlu/route.ts
@@ -0,0 +1,42 @@
+import { vluLoginURL } from '@/lib/urls'
+import { createFormData, getVluCookie } from '@/lib/vlu'
+import { NextRequest } from 'next/server'
+
+/**
+ * Handles the GET request to login to VLU.
+ *
+ * @param req - The request object containing `id` and `password` as query parameters.
+ * @returns A Response object with the login cookie if successful, or an error message.
+ *
+ * @example
+ * // Request URL: /api/accounts/vlu?id=yourId&password=yourPassword
+ * // Success response: { "cookie": "yourVluCookie" }
+ * // Failure response: { "error": "Failed to fetch vlu cookie" }
+ */
+
+export async function GET(req: NextRequest) {
+  const { id, password } = Object.fromEntries(new URL(req.url).searchParams)
+
+  if (!id || !password) return new Response('Missing id or password', { status: 400 })
+
+  const vluCookie = await getVluCookie()
+  if (!vluCookie) return Response.json({ error: 'Failed to fetch vlu cookie' }, { status: 503 })
+
+  // login to vlu
+  const header = new Headers()
+  header.append('Cookie', vluCookie)
+
+  // thanks to @PhucChiVas161 for the advice!
+  const loginResponse = await fetch(vluLoginURL, {
+    method: 'POST',
+    headers: header,
+    body: await createFormData(id, password),
+    redirect: 'manual',
+  })
+
+  console.log(loginResponse)
+
+  if (loginResponse.status !== 302) return Response.json({ error: 'Failed to login to VLU' }, { status: 503 })
+
+  return Response.json({ cookie: vluCookie }, { status: 200 })
+}

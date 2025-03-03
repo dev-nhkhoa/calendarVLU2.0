341b8563e7e8aaeb64d5fec9f0737b60799ba5a2
nhkhoa <work.nkkhoa@gmail.com>
2025-03-03 21:12:21 +0700

    Add @vercel/speed-insights package and integrate SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { AppProvider } from '@/app-provider'
 import { ToastContainer } from 'react-toastify'
 
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </AppProvider>
         </SessionProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )

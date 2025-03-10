22fd8d69d4e5bbcc99ce6cde4ebea9127d54702d
nhkhoa <work.nkkhoa@gmail.com>
2025-03-10 22:35:26 +0700

    Add Privacy Policy page and update footer with link to privacy policy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function FooterPage() {
   return (
     <footer className="py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <p className="text-center">
+        Xem{' '}
+        <Link href="/privacy-policy" className="text-blue-400 underline">
+          Chính sách bảo mật
+        </Link>{' '}
+        của chúng tôi để biết thêm thông tin về cách chúng tôi xử lý dữ liệu của bạn.
+      </p>
       <p className="text-xs text-center text-gray-500 dark:text-gray-400">© 2025 calendarVLU. All rights reserved.</p>
     </footer>
   )
